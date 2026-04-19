0f8fac8111e50c9bb18e1b377328b417ecbd24db
Ervin Senkevich <146520901+Ervin518@users.noreply.github.com>
2026-04-19 16:25:05 +0200

    Refactor filter condition using 'and_' operator

--- lab_4/lab_4_zad_2.py
+++ lab_4/lab_4_zad_2.py
@@ -1,10 +1,10 @@
 import random
-from operator import lt, mod, eq
+from operator import lt, mod, eq, and_
 
-list_1 = [random.randint(1, 10001) for _ in range(10000)]
+list_1 = [random.randint(1, 10000) for _ in range(10000)]
 
 mniejsze_i_parzyste = list(
-filter(lambda x: lt(x, 3) and eq(mod(x, 2), 0), list_1)
+    filter(lambda x: and_(lt(x, 3), eq(mod(x, 2), 0)), list_1)
 )
 
 print(mniejsze_i_parzyste)
