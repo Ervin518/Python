741a032747249bb91cf75ddcef8093f394e8aa27
Ervin Senkevich <146520901+Ervin518@users.noreply.github.com>
2026-04-19 16:49:26 +0200

    Update lab_5_zad_3.py

--- lab_5_6/lab_5_zad_3.py
+++ lab_5_6/lab_5_zad_3.py
@@ -1,4 +1,13 @@
 def potegi(*args):
+    """
+    Funkcja oblicza x^x dla kazdego podanego argumentu.
+    
+    Args:
+        *args: Dowolna liczba argumentow.
+        
+    Returns:
+        list: Lista wynikow x^x.
+    """
     if len(args) >= 100:
         print("Za duzo argumentow")
     elif len(args) == 0:
@@ -6,10 +15,16 @@ def potegi(*args):
     else:
         wyniki = []
         for arg in args:
-            x = float(arg)
-            wyniki.append(x ** x)
+            try:
+                x = float(arg)
+                wyniki.append(x ** x)
+            except ValueError:
+                print("Element '" + str(arg) + "' nie jest liczba. Pomijam.")
         return wyniki
 
-argumenty = input("Podaj argumenty: ")
-wynik_koncowy = argumenty.split(",")
-print("Wyniki: ", potegi(*wynik_koncowy))
+argumenty = input("Podaj argumenty po przecinku: ")
+if argumenty.strip() != "":
+    wynik_koncowy = argumenty.split(",")
+    print("Wyniki: ", potegi(*wynik_koncowy))
+else:
+    print("Brak argumentow")
