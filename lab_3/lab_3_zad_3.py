e8dc6e3199ca4b295648494ec9d667fa38c10e9a
Ervin Senkevich <146520901+Ervin518@users.noreply.github.com>
2026-04-19 15:24:23 +0200

    Refactor letter search into a lambda function

--- lab_3/lab_3_zad_3.py
+++ lab_3/lab_3_zad_3.py
@@ -1,7 +1,6 @@
+szukaj_litery = lambda wyraz, litera: print('Jest taka litera') if litera in wyraz else print('Nie ma takiej litery')
+
 a = input('Podaj dowolny wyraz: ')
 b = input('Podaj dowolna litere: ')
 
-if b in a:
-    print('Jest taka litera')
-else:
-    print('Nie ma takiej litery')
+szukaj_litery(a, b)
