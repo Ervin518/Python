32c61edd0a2bdbb92ae1e376e75fc1530d46fd9a
Ervin Senkevich <146520901+Ervin518@users.noreply.github.com>
2026-04-19 16:42:20 +0200

    Update lab_2_zad_2.py

--- lab_2/lab_2_zad_2.py
+++ lab_2/lab_2_zad_2.py
@@ -2,10 +2,12 @@ lista_gene1 = ['SLC19A2', 'ATP7B', 'ERBB3', 'FGFR14', 'ABCC3','GALNT14', 'ERCC1'
                 'LJS19A2', 'AKM7B', 'ELLB34', 'FULR4', 'ANGC3', 'WELNT14', 'EOO1',
                 'SAC19A22', 'FGFR4', 'ERB3', 'FGR4', 'FGFR4', 'GASNT14', 'ERSS4']
 
-if 'FGFR4' in lista_gene1 and 'FGERA4' in lista_gene1:
-    ind = lista_gene1.index('FGFR4')
-    print(ind)
-    ind2 = lista_gene1.index('FGERA4')
-    print(ind2)
+if 'FGFR4' in lista_gene1:
+    print('Gen FGFR4 znajduje sie na indeksie:', lista_gene1.index('FGFR4'))
 else:
-    print('Nie ma takich elementow')
+    print('Brak genu FGFR4')
+
+if 'FGERA4' in lista_gene1:
+    print('Gen FGERA4 znajduje sie na indeksie:', lista_gene1.index('FGERA4'))
+else:
+    print('Brak genu FGERA4')
